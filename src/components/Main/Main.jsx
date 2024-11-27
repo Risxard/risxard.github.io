af220a82e04f5585557f7e7eeebdf07fe3efaf1a
Richard <richadson@ymail.com>
2024-11-27 14:01:27 -0300

    add: about me section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
-import { getAllProjects } from "../../lib/dato-cms";
 import IntroMain from "./IntroMain/IntroMain";
 import Portfolio from "./Portfolio/Portfolio";
 import Carreira from "./Carreira/Carreira";
 import Skills from "./Skills/Skills";
+import AboutMe from "./AboutMe/AboutMe";
 
 const Main = () => {
   return (
     <main>
       <IntroMain />
+      <AboutMe/>
       <Portfolio />
       <Skills />
       <Carreira />
